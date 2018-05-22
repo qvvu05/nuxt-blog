53656acd96b88c11d49cf1ea1ebc1d3f4279cc6b
Quyen <qvvu05@gmail.com>
2018-05-22 16:52:36 +0700

    initialize file

---
+++ pages/about/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <section>
+        <h1>Hello, This is about page</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis voluptas enim totam deleniti aperiam, nobis aliquam maxime exercitationem laboriosam impedit placeat modi non consequatur, optio assumenda illum excepturi. Eius, autem!</p>
+    </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="container">
+    <nuxt-link>
+      <article class="post-preview"></article>
+    </nuxt-link>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
